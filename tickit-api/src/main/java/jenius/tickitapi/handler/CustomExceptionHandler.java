1ab18533809fc53b8544bcb28b550ea4c3556887
JinjinBread <pge0720@naver.com>
2025-03-12 20:19:55 +0900

    build: add common-exception dependency

--- tickit-api/src/main/java/jenius/tickitapi/handler/CustomExceptionHandler.java
+++ tickit-api/src/main/java/jenius/tickitapi/handler/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package jenius.tickitapi.handler;
 
+import jenius.commonexception.CustomException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
