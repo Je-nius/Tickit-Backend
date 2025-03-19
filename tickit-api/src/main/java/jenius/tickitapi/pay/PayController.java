8404ba37aaf1c2c5bf3d33441cd96a131edd4d98
JinjinBread <pge0720@naver.com>
2025-03-19 10:37:41 +0900

    chore: update payment api

--- tickit-api/src/main/java/jenius/tickitapi/pay/PayController.java
+++ tickit-api/src/main/java/jenius/tickitapi/pay/PayController.java
@@ -10,19 +10,18 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/payment")
 @RequiredArgsConstructor
 public class PayController {
 
     private final KakaoPayService kakaoPayService;
 
-    @PostMapping("/ready")
+    @PostMapping("/api/ticket/pay")
     public ResponseEntity<KakaoPayReadyResponseDto> readyForKakaoPay(@RequestBody KakaoPayReadyRequestDto requestDto) {
         return ResponseEntity.ok()
                 .body(kakaoPayService.readyForKakaoPay(requestDto));
     }
 
-    @GetMapping("/approve")
+    @GetMapping("/api/pay/approve")
     public ResponseEntity<KakaoPayApproveResponseDto> approveForKakaoPay(@RequestParam(name = "order_id") String orderId,
                                                                          @RequestParam(name = "user_id") String userId,
                                                                          @RequestParam(name = "pg_token") String pgToken) {
@@ -30,25 +29,12 @@ public class PayController {
                 .body(kakaoPayService.approveForKakaoPay(pgToken, orderId, userId));
     }
 
+    /* TODO
     @PostMapping("/cancel")
     public ResponseEntity<KakaoPayReadyResponseDto> cancelForKakaoPay(@RequestBody KakaoPayReadyRequestDto requestDto) {
         return ResponseEntity.ok()
-                .body(kakaoPayService.readyForKakaoPay(requestDto));
+                .body(kakaoPayService.cancelForKakaoPay(requestDto));
     }
-
-    @GetMapping("/success")
-    public String successReady() {
-        return "ready success";
-    }
-
-    @GetMapping("/cancel")
-    public String cancelReady() {
-        return "cancel ready";
-    }
-
-    @GetMapping("/fail")
-    public String failReady() {
-        return "fail ready";
-    }
+     */
 
 }
